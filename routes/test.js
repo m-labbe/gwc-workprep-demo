9c2731dcd2f80aaa1ac20546711e22bf6dd0d314
Melissa Labbe <melissa-labbe@pluralsight.com>
2023-06-05 15:05:46 -0400

    add test for user route

---
+++ routes/test.js
@@ -0,0 +1,21 @@
+/**
+ * @jest-environment node
+ */
+
+const request = require("supertest");
+const app = require("../app");
+
+describe('All-skills page bootstrap', () => {
+
+    it('Gets all users data', (done) => {
+        request(app)
+            .get('/api/users')
+            .expect(200)
+            .expect((result) => {
+                expect(result.body[0].email).toBe("psuser@example.com");
+                expect(result.body[0].username).toBe("psuser");
+            })
+            .end(done);
+    });
+
+});
