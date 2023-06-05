f4d3710568eefae1bc901f4d27113ae5dc0833c2
Erik Gross <erik.d.gross@gmail.com>
2023-06-05 15:23:41 -0500

    get user by id works

--- routes/users.js
+++ routes/users.js
@@ -10,5 +10,15 @@ router.get("/users", async (req, res) => {
   res.send(users);
 });
 
+router.get("/users/:id", async (req, res) => 
+{
+  const userid = parseInt(req.params.id);
+  if(isNaN(userid)) 
+  {
+    res.send({"error": "Specified User not found"});
+  }
+  var user = users.find(item => item.id === userid);
+  res.send(user);
+})
 
 module.exports = router;
